ad96e8bb1e73d7e1f1eb4a886968432cd8b4d7d0
Joost van Breukelen <Joost@imac-van-joost.fritz.box>
2016-09-18 20:18:51 +0200

    new colors

--- JvbForceTouchDemo/ViewController.swift
+++ JvbForceTouchDemo/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var forceTouchView: ForceTouchView!
     
-    let maxScaleFactor:CGFloat = 0.2
+    let maxScaleFactor:CGFloat = 0.3
+    var feedbackLayer = CALayer()
+    var orangeLayer = CALayer()
+    var pressed: Bool = false {
+        
+        willSet{
+            orangeLayer.backgroundColor = newValue ? UIColor.init(colorLiteralRed: 255/255, green: 107/255, blue: 40/255, alpha: 1).cgColor : UIColor.orange.cgColor
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +34,33 @@ class ViewController: UIViewController {
         
         let forceTouchRecognizer = ForceGestureRecognizer(target: self, action: #selector(forceTouchAction(gesture:)))
         forceTouchView.addGestureRecognizer(forceTouchRecognizer)
+        
+        forceTouchView.layer.cornerRadius = 20
+        orangeLayer.cornerRadius = 20
+        feedbackLayer.cornerRadius = 20
+        
+        feedbackLayer.backgroundColor = UIColor.orange.cgColor
+        orangeLayer.backgroundColor = UIColor.orange.cgColor
+        feedbackLayer.opacity = 0.8
+
+        forceTouchView.layer.addSublayer(orangeLayer)
+        forceTouchView.layer.insertSublayer(feedbackLayer, below: orangeLayer)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        
+        orangeLayer.frame = forceTouchView.bounds
+        feedbackLayer.frame = forceTouchView.bounds
+        
     }
 
     func longPressAction(gesture: UILongPressGestureRecognizer) {
@@ -47,30 +77,29 @@ class ViewController: UIViewController {
     
     func forceTouchAction(gesture: ForceGestureRecognizer) {
         
-        print("maxValue is \(gesture.maxValue)")
-        print(gesture.forceValue)
+        print("force applied: \(gesture.forceValue)")
         if gesture.forceValue > 1{
             
-                let scaleFactor = 1 + (((gesture.forceValue - 1) / gesture.maxValue) * maxScaleFactor)
+            pressed = true
+            let scaleFactor = 1 + (((gesture.forceValue - 1) / gesture.maxValue) * maxScaleFactor)
+            feedbackLayer.transform = CATransform3DMakeScale(scaleFactor, scaleFactor, 1)
             
-                let scale = CGAffineTransform(scaleX: scaleFactor ,y: scaleFactor)
-                forceTouchView.transform = scale
-            
-            if gesture.forceValue > 5{
+            if gesture.forceValue > 6{
                 AudioServicesPlaySystemSound(1520)
                 gesture.isEnabled = false
-                let scale = CGAffineTransform(scaleX: 1, y: 1)
-                forceTouchView.transform = scale
+                feedbackLayer.transform = CATransform3DMakeScale(1, 1, 1)
                 gesture.isEnabled = true
+                pressed = false
             }
         }else{
-            let scale = CGAffineTransform(scaleX: 1, y: 1)
-            forceTouchView.transform = scale
+            feedbackLayer.transform = CATransform3DMakeScale(1, 1, 1)
+
         }
         
         if gesture.state == .ended{
-            let scale = CGAffineTransform(scaleX: 1, y: 1)
-            forceTouchView.transform = scale
+            feedbackLayer.transform = CATransform3DMakeScale(1, 1, 1)
+            pressed = false
+
 
         }
     }
